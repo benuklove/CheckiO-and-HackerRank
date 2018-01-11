a0f6db6f931bf0c4515e20f1f77afc953ced24c6
benlove <benuklove@gmail.com>
2018-01-10 19:54:24 -0600

    CheckiO: between_markers - using text.find()

--- best_stock.py
+++ best_stock.py
@@ -1,18 +1,39 @@
+from timeit import timeit
+
+
 def best_stock(data):
-    # your code here
     maxx = 0
     best = ""
     for key, value in data.items():
-        print(key, ":", value)
         if value > maxx:
             maxx = value
             best = key
     return best
 
 
-print("Example:")
-print(best_stock({
-        'CAC': 10.0,
-        'ATX': 390.2,
-        'WIG': 1.2
-    }))
+def slower_better_stock(data):
+    # your code here
+    return max(data, key=data.get)
+
+
+# print(timeit(best_stock({
+#         'CAC': 10.0,
+#         'ATX': 390.2,
+#         'WIG': 1.2
+#     }), number=1000))
+
+
+stocks = {
+    'CAC': 10.0,
+    'ATX': 390.2,
+    'WIG': 1.2
+}
+
+if __name__ == "__main__":
+    print(timeit("best_stock(stocks)",
+                 "from __main__ import best_stock, stocks",
+                 number=1000000),
+          timeit("slower_better_stock(stocks)",
+                 "from __main__ import slower_better_stock, stocks",
+                 number=1000000)
+          )
